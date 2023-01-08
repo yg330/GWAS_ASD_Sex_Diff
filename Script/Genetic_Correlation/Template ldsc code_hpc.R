774afe270dfa0ebc2ffa6570a4e9141271eb162b
yg330 <119585142+yg330@users.noreply.github.com>
2023-01-08 17:42:26 +0000

    Added directory location and conda set up

--- Script/Genetic_Correlation/Template ldsc code_hpc.R
+++ Script/Genetic_Correlation/Template ldsc code_hpc.R
@@ -1,2 +1,4 @@
 # Template code for running ldsc on hpc
-ldsc.py --h2 ./sumstats/MD.sumstats --ref-ld-chr eur_w_ld_chr/ --w-ld-chr eur_w_ld_chr/ --out /rds/user/yg330/hpc-work/testing
+cd /rds/project/rds-Nl99R8pHODQ/ldsc/
+conda activate ldsc
+ldsc.py --h2 ./sumstats/MD.sumstats --ref-ld-chr eur_w_ld_chr/ --w-ld-chr eur_w_ld_chr/ --out /rds/user/yg330/hpc-work/ASDF_ASDM
